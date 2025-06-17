2a33c6946d5a72eaaf513f7cc35893e104eba33c
zhongweixin <1299037599@qq.com>
2025-06-17 15:00:58 +0800

    单向循坏链表（完成）

--- C++_algorithm/02_link_circle.cpp
+++ C++_algorithm/02_link_circle.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
 
 class CircleLink
@@ -7,7 +8,7 @@ public:
     CircleLink()
     {
         head = new Node();
-        head = tail;
+        tail = head;
         head->next = head;
     }
     ~CircleLink()
@@ -20,6 +21,81 @@ public:
             p = head->next;
         }
         delete head;
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    // 尾插法 O(1)
+    void InsertTail(int val)
+    {
+        Node* node = new Node(val);
+        node->next = tail->next;
+        tail->next = node;
+        tail = node; // 更新指针tail指向新的尾节点
+    }
+
+    // 头插法
+    void InsertHead(int val)
+    {
+        Node* node = new Node(val);
+        node->next = head->next;
+        head->next = node;
+        if(node->next == head)
+        {
+            tail = node;
+        }
+    }
+
+    // 删除节点
+    void Remove(int val)
+    {
+        Node* q = head;
+        Node* p = head->next;
+        while(p != head)
+        {
+            if(p->data == val)
+            {
+                q->next = p->next;
+                delete p;
+                if(q->next == head)
+                {
+                    tail = q;
+                }
+                return;
+            }
+            else
+            {
+                p = p->next;
+                q = q->next;
+            }
+        }
+    }
+
+    //查询
+    bool Find(int val) const
+    {
+        Node* p = head->next;
+        while(p!= head)
+        {
+            if(p->data == val)
+            {
+                return true;
+            }
+            p = p->next;
+        }
+        return false;     
+    }
+
+    //打印
+    void Show() const
+    {
+        Node* p = head->next;
+        while(p!= head)
+        {
+            cout << p->data << " ";
+            p = p->next;
+        }
+        cout << endl;
     }
 
 private:
@@ -36,6 +112,36 @@ private:
 
 int main()
 {
+    srand(time(0));
+    CircleLink clink;
+
+    clink.InsertHead(1000);
+    clink.Show();
+
+    for(int i =0; i<10;i++)
+    {
+        clink.InsertTail(rand()%100);
+    }
+    clink.Show();
+
+    clink.InsertTail(24);
+    clink.Show();
+
+    clink.InsertHead(888);
+    clink.Show();
+
+    clink.Remove(1000);
+    clink.Show();
+
+    if(clink.Find(81))
+    {
+        cout << "找到了。" << endl;
+    }
+    else
+    {
+        cout << "没有找到。" << endl;
+    }
+
     return 0;
 }
 
