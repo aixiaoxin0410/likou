6aead9e2c86dd8378a335d81af2da2c586954f2d
zhongweixin <1299037599@qq.com>
2025-09-09 20:49:06 +0800

    BST树非递归删除

---
+++ C++_BST_tree/02_BST_del.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+using namespace std;
+#include <functional>  // 添加这个头文件
+
+// BST树代码实现
+template<typename T, typename Compare=less<T>>
+class BSTree
+{
+public:
+    BSTree()
+        : root_(nullptr)
+        {}
+    ~BSTree(){}
+
+    // 非递归插入操作
+    void n_insert(const T &val)
+    {
+        // 树为空，生成根节点
+        if(root_ == nullptr)
+        {
+            root_ = new Node(val);
+            return;
+        }
+
+        // 搜索合适的插入位置，记录父节点的位置
+        Node* parent = nullptr;
+        Node* cur = root_;
+        while(cur!= nullptr)
+        {
+            if(cur->data_ == val) 
+            {
+                // 不插入元素相同的值
+                return;
+            }
+            else if(comp_(cur->data_,val)) // 小于关系
+            {
+                parent = cur;
+                cur = cur->right_;
+            }
+
+            else
+            {   
+                parent = cur;
+                cur = cur->left_;
+            }
+        }
+
+        // 把新节点插入到parent节点的孩子上
+        if(comp_(parent->data_,val)) // 小于关系
+        {
+            parent->right_ = new Node(val);
+        }
+        else
+        {
+            parent->left_= new Node(val);
+        }
+    }
+
+    // 非递归删除操作
+    void n_remove(const T& val)
+    {
+        if(root_ == nullptr)
+        {
+            return;
+        }
+
+        Node* parent = nullptr;
+        Node* cur = root_;
+        while(cur!= nullptr)
+        {
+            if(cur->data_ == val) 
+            {
+                break; // 找到待删除节点
+            }
+            else if(comp_(cur->data_,val)) // 小于关系
+            {
+                parent = cur;
+                cur = cur->right_;
+            }
+            else
+            {   
+                parent = cur;
+                cur = cur->left_;
+            }
+        }
+
+        // 没找到待删除节点
+        if(cur == nullptr)
+        {
+            return;
+        }
+
+        // 情况3
+        if(cur->left_!=nullptr && cur->right_!=nullptr)
+        {
+            parent = cur;
+            Node* pre = cur->left_;
+            while (pre->right_ !=nullptr)
+            {
+                parent = pre;
+                pre = pre->right_;
+            }
+            
+            cur->data_ = pre->data_;
+            cur = pre; // 让cur指向前驱节点，转化成情况1，2
+        }
+
+        // cur指向删除节点，parent指向其父节点，统一处理情况1或者2
+        Node* child = cur->left_;
+        if(child == nullptr)
+        {
+            child = cur->right_;
+        }
+
+        if(parent == nullptr) // 特殊情况：表示删除的是根节点
+        {
+            root_ = child;
+        }
+
+        // 把待删除节点的孩子（nullptr或者不空）写入其父节点相应地址域中
+        if(parent->left_ == cur)
+        {
+            parent->left_ = child;
+        }
+        else
+        {
+            parent->right_ = child;
+        }
+
+        delete cur; // 删除当前节点
+    }
+
+private:
+    struct Node
+    {
+        Node(T data = T())
+            : data_(data)
+            , right_(nullptr)
+            , left_(nullptr)
+            {}
+        T data_; // 数据域
+        Node* right_; // 左孩子域
+        Node* left_; // 右孩子域
+    };
+    
+    Node* root_; // 指向BST树的根节点
+    Compare comp_; // 定义一个函数对象
+};
+
+int main()
+{
+    int arr[] = {58,24,67,0,34,62,69,5,41,64,78};
+    BSTree<int> bst;
+    for(auto v:arr)
+    {
+        bst.n_insert(v);
+    }
+
+    bst.n_insert(12);
+    return 0;
+}
