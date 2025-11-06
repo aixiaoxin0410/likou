a10bb939c185a10a4d67061ff274455e400f1fa8
zhongweixin <1299037599@qq.com>
2025-11-06 21:28:52 +0800

    回溯算法01

---
+++ C++_Classic_algorithm/01Backtracking_algorithm/01.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void func(int arr[], int i, int length)
+{
+    if(i == length) // 递归结束条件
+    {
+        for(int j = 0 ; j<length; j++)
+        {
+            cout << arr[j] << " ";
+        }
+        cout << endl;
+    }
+    else
+    {
+        func(arr,i+1,length);
+        func(arr,i+1,length);
+    }
+}
+
+int main()
+{
+    int arr[] = {1,2,3};
+    int length = sizeof(arr)/sizeof(arr[0]);
+    func(arr,0,length);
+    return 0;
+}
